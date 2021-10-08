d5392f07d8446ec9e953da83d6e377c4177dfb4d
Tanner Stratford <tannerstratford1@gmail.com>
2021-10-08 14:21:33 -0600

    pushing

--- src/components/WorkoutModel/app.jsx
+++ src/components/WorkoutModel/app.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import "./app.css";
 
 function WorkoutModel(props) {
- return (
- <div className="workout">
-     <table class="ui celled table">
-     <thead>
-         <tr>
-         <th>Workout</th>
-         <th>Video Link</th>
-         </tr>
-     </thead>
-     <tbody>
-        <tr>
-            <td>{props.name}</td>
-            <td>{props.url}</td>
-        </tr>
+    return (
+        <tbody>
+            <tr>
+                <td>{props.name}</td>
+                <td> <a href={props.url} target = "_blank">Video Link</a></td>
+            </tr>
         </tbody>
-        </table>
- </div>
- );
+    );
 }
 
 export default WorkoutModel;
