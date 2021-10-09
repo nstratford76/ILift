51f18f728f06d56f6982ac2be2b7a6f285f26a60
Nathan Stratford <nstratford76@gmail.com>
2021-10-09 16:10:59 -0600

    Took off video column and videos appear when clicking name column

--- src/components/WorkoutModel/app.jsx
+++ src/components/WorkoutModel/app.jsx
@@ -5,10 +5,9 @@ function WorkoutModel(props) {
     return (
         <tbody className="workoutTable">
             <tr>
-                <td>{props.name}</td>
+                <td onClick={() => props.changeUrl(props.url)}>{props.name}</td>
                 <td> <p> <span>Set 1: {props.set1Reps}</span><br></br><span>Set 2: {props.set2Reps}</span><br></br> <span>Set 3: {props.set3Reps}</span> </p> </td>
                 <td> <p> <span>Set 1: <input type="text" /> </span><br></br><span>Set 2: </span><br></br><span>Set 3: </span> </p>  </td>
-                <td> <iframe src={props.url}>Video Link</iframe></td>
             </tr>
         </tbody>
     );
