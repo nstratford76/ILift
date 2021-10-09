0b6da17ff53c3b3f4559a3b3eb240a2bcbc77f7c
Josh Betzsold <bet16011@byui.edu>
2021-10-09 15:36:47 -0600

    josh update

--- src/components/WorkoutModel/app.jsx
+++ src/components/WorkoutModel/app.jsx
@@ -5,9 +5,9 @@ function WorkoutModel(props) {
     return (
         <tbody className="workoutTable">
             <tr>
-                <td>{props.name}</td>
-                <td> <p> <span>Set 1: {props.set1Reps}</span><br></br><span>Set 2: {props.set2Reps}</span><br></br> <span>Set 3: {props.set3Reps}</span> </p> </td>
-                <td> <p> <span>Set 1: <input type="text" /> </span><br></br><span>Set 2: </span><br></br><span>Set 3: </span> </p>  </td>
+                <td className="movement">{props.name}</td>
+                <td className="sets"> <p> <span>Set 1: {props.set1Reps}</span><br></br><span>Set 2: {props.set2Reps}</span><br></br> <span>Set 3: {props.set3Reps}</span> </p> </td>
+                <td className="weight"> <p> <span>Set 1: <input type="text" /> </span><br></br><span>Set 2: </span><br></br><span>Set 3: </span> </p>  </td>
                 <td> <iframe src={props.url}>Video Link</iframe></td>
             </tr>
         </tbody>
