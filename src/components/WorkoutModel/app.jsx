49ce7831611f460d90f9cd06e0bea4d0d00cb830
Tanner Stratford <tannerstratford1@gmail.com>
2021-10-08 18:29:10 -0600

    Changes

--- src/components/WorkoutModel/app.jsx
+++ src/components/WorkoutModel/app.jsx
@@ -6,8 +6,8 @@ function WorkoutModel(props) {
         <tbody>
             <tr>
                 <td>{props.name}</td>
-                <td> <input type="text" />Reps </td>
-                <td> <input type="text" />Weight </td>
+                <td> Set 1: <input type="text" /> Set 2: <input type="text" /> Set 3: <input type="text" /> </td>
+                <td> Set 1: <input type="text" /> Set 2: <input type="text" /> Set 3: <input type="text" /> </td>
                 <td> <iframe src={props.url}>Video Link</iframe></td>
             </tr>
         </tbody>
